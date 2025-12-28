d67fa9242ae36467f988226b0f6354b0dbf094cd
sillas <deepsky.sill@gmail.com>
2025-12-28 17:08:14 -0300

    refactor: update header to display document slug without additional text

--- components/editor/Header.tsx
+++ components/editor/Header.tsx
@@ -6,6 +6,6 @@ interface EditorHeaderProps {
 export default function EditorHeader({ slug }: EditorHeaderProps) {
   const isOnline = useIsOnline();
     return (<header className="w-full h-14 bg-slate-700 flex items-center px-4 z-10">
-        <div className="text-white text-sm">Document {slug}{isOnline ? '' : ' - Offline'}</div>
+        <div className="text-white text-sm">{slug}{isOnline ? '' : ' - Offline'}</div>
       </header>);
 }``
